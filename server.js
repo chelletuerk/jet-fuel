379442c45e9f9a5ba4ad0c65ea4c5466b24f8105
Elijah Williams <ejwill04@gmail.com>
2017-03-14 15:55:44 -0600

    Move resources to public folder and add to server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.set('port', process.env.PORT || 3000)
 
