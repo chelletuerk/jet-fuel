b31e014271607d18edb0359db17f3263e8cde8cf
Elijah Williams <ejwill04@gmail.com>
2017-03-14 15:28:50 -0600

    Modify obj structure and add md5

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const md5 = require('md5')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -8,24 +9,38 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000)
 
 app.locals.folders = [
-  { 'folder1': [{
+  { id: 1,
+    name: 'fruit'
+  },
+  { id: 2,
+    name: 'penguins',
+  }
+]
+
+app.locals.urls = [
+  { folderId: 1,
     submitDate: '03142017',
     numOfClicks: '3',
     shortenedUrl: 'http://www.g.com',
     url: 'http://www.google.com'
-    }]
   },
-  { 'folder2': [{
+  { folderId: 2,
     submitDate: '03152017',
     numOfClicks: '1',
     shortenedUrl: 'http://www.a.com',
     url: 'http://www.amazon.com'
-    }]
   }
 ]
 
 app.get('/api/v1/folders', (request, response) => {
   response.send(app.locals.folders)
+})
+
+app.post('/api/v1/folders', (request, response) => {
+  const { folder } = request.body
+
+  app.locals.folders.push({folder: []})
+  response.json(app.locals.folders)
 })
 
 app.listen(app.get('port'), () => {
