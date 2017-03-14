b131d7587a5dae55dfd4a7adcda32e657b6ef7af
chelle tuerk <michelletuerk@gmail.com>
2017-03-14 14:43:38 -0600

    installed express and body parser

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
-const http = require("http");
-
-
-const server = http.createServer()
-
-server.listen(3000, () => {
-  console.log('The HTTP server is listening at Port 3000.');
-});
-
-server.on('request', (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello World');
-  response.end();
-});
-
-const port = process.env.PORT || 3000;
-
-server.listen(port);
-console.log(`Listening on ${port}`);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 3000)
+// app.locals.folders = []
+// folder = { id: 1, name: food }
+// app.locals.folders.push.(folder)
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
