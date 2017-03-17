c27d66371da47922b83f75872fec555495af6d09
Elijah Williams <ejwill04@gmail.com>
2017-03-17 12:10:19 -0600

    Handle linter errors

--- public/index.js
+++ public/index.js
@@ -1,6 +1,3 @@
-// const $folderContainer = $('.folder-container');
-//
-// const folderArray = [];
 let clickedFolder;
 let currentUrls;
 let sortOrder = false
@@ -11,26 +8,26 @@ $('.folder-input').focus();
 
 $('.folder-input').on('input', (e) => {
   e.target.value.length > 0 ? $('.folder-submit').prop('disabled', false) : $('.folder-submit').prop('disabled', true);
-})
+});
 
 $('.folder-submit').on('click', () => {
   const $folderName = $('.folder-input').val();
   addFolderToList($folderName);
   $('.folder-input').val('');
-})
+});
 
 $('.folder-container').on('click', '.folder-button', (e) => {
   clickedFolder = e.target.id;
-  styleFolderOnClick(e)
+  styleFolderOnClick(e);
   $('.url-section').children().attr('disabled', false);
-  $('.url-container').children().remove()
-  loadInitialUrls(clickedFolder)
-})
+  $('.url-container').children().remove();
+  loadInitialUrls(clickedFolder);
+});
 
 const styleFolderOnClick = (e) => {
   $(e.target).siblings().removeAttr('style');
   $(e.target).css('background-color', '#ff8811');
-}
+};
 
 $('.url-button').on('click', () => {
   const url = $('.url-input').val();
@@ -39,12 +36,12 @@ $('.url-button').on('click', () => {
     alert("Your URL isn't valid. Try something like \n http://www.ebaumsworld.com/");
     return;
   }
-  postUrl(url)
+  postUrl(url);
   $('.url-input').val('');
-})
+});
 
 $('.sort-date').on('click', () => {
-  $('.url-container').children().remove()
+  $('.url-container').children().remove();
   if (!sortOrder) {
     renderUrls(downSort('timestamp'));
     sortOrder = !sortOrder;
@@ -52,7 +49,7 @@ $('.sort-date').on('click', () => {
     renderUrls(upSort('timestamp'));
     sortOrder = !sortOrder;
   }
-})
+});
 
 $('.sort-clicks').on('click', () => {
   $('.url-container').children().remove()
@@ -63,10 +60,10 @@ $('.sort-clicks').on('click', () => {
     renderUrls(upSort('numOfClicks'));
     sortOrder = !sortOrder;
   }
-})
+});
 
 function upSort(prop) {
-  return currentUrls.sort((a, b) => a[prop] > b[prop])
+  return currentUrls.sort((a, b) => a[prop] > b[prop]);
 }
 
 function downSort(prop) {
@@ -76,7 +73,7 @@ function downSort(prop) {
 const validateUrl = (url) => {
   const urlRegex = /^(http|https)?:\/\/[w]{2,4}[a-zA-Z0-9-\.]+\.[a-z]{1,10}/
   return urlRegex.test(url);
-}
+};
 
 const loadInitialFolders = () => {
   fetch(`/api/v1/folders`, {
@@ -84,10 +81,10 @@ const loadInitialFolders = () => {
   })
   .then(response => response.json())
   .then(data => {
-    renderFolders(data)
+    renderFolders(data);
   })
-  .catch(err => err)
-}
+  .catch(err => err);
+};
 
 const loadInitialUrls = () => {
   fetch(`/api/v1/urls`, {
@@ -96,13 +93,12 @@ const loadInitialUrls = () => {
   .then(response => response.json())
   .then(data => {
 
-    renderUrls(data, clickedFolder)
+    renderUrls(data, clickedFolder);
   })
-  .catch(err => console.log('error', err))
-}
+  .catch(err => console.log('error', err));
+};
 
 const addFolderToList = (name) => {
-  console.log('name', name)
   fetch(`/api/v1/folders`, {
     headers: {
       'Content-Type': 'application/json'
@@ -111,10 +107,10 @@ const addFolderToList = (name) => {
     body: JSON.stringify({ name })
   })
   .then(response => response.json()).then(data => {
-    renderFolders([data[data.length-1]])
+    renderFolders([data[data.length-1]]);
   })
-  .catch(err => 'err')
-}
+  .catch(err => 'err');
+};
 
 const postUrl = (url) => {
   fetch(`/api/v1/urls`, {
@@ -125,25 +121,25 @@ const postUrl = (url) => {
     body: JSON.stringify({ folderId: clickedFolder, url })
   })
   .then(response => response.json()).then(data => {
-    currentUrls = data
-    renderUrls([data[data.length-1]])
+    currentUrls = data;
+    renderUrls([data[data.length-1]]);
   })
-  .catch(err => 'err')
-}
+  .catch(err => 'err');
+};
 
 const renderFolders = (data) => {
   data.map(obj => {
     $('.folder-container').append(`
       <button class="folder-button" id=${obj.id}>${obj.name}
       </button>
-      `)
-  })
-}
+      `);
+  });
+};
 
 const renderUrls = (data, clickedFolder) => {
   if (clickedFolder) {
-    data = data.filter(obj => obj.folderId == clickedFolder)
-    currentUrls = data
+    data = data.filter(obj => obj.folderId == clickedFolder);
+    currentUrls = data;
   }
 
   data.map(obj => {
@@ -157,13 +153,13 @@ const renderUrls = (data, clickedFolder) => {
         <div class="url-numOfClicks">${obj.numOfClicks}</div>
         <div class="url-longUrl">${obj.url}</div>
       </div>
-      `)
-  })
-}
+      `);
+  });
+};
 
 $('.url-container').on('click', '.shortenUrlBtn', (e) => {
-  const shortUrl = e.target.innerHTML.trim()
+  const shortUrl = e.target.innerHTML.trim();
   fetch(`/${shortUrl}`, {
     method: 'PUT'
-  })
-})
+  });
+});
--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const md5 = require('md5')
-const fs = require('fs')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const md5 = require('md5');
+const fs = require('fs');
+let count = 0;
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
   fs.readFile(`${__dirname}/index.html`, (err, file) => {
-    response.send(file)
-  })
-})
+    response.send(file);
+  });
+});
 
 app.get('/api/v1/folders', (request, response) => {
   database('folders').select()
   .then((folders) => {
-    response.status(200).json(folders)
+    response.status(200).json(folders);
   })
   .catch((error) => {
-    console.error('error getting folders:', error)
-  })
-})
+    console.error('error getting folders:', error);
+  });
+});
 
 app.post('/api/v1/folders', (request, response) => {
-  const { name } = request.body
+  const { name } = request.body;
   const folder = { name };
   database('folders').insert(folder)
   .then(function() {
@@ -40,28 +41,28 @@ app.post('/api/v1/folders', (request, response) => {
               response.status(200).json(folders);
             })
             .catch(function(error) {
-              console.error('error posting folders:', error)
+              console.error('error posting folders:', error);
             });
-  })
-})
+  });
+});
 
 app.get('/api/v1/urls', (request, response) => {
   database('urls').select()
   .then((urls) => {
-    response.status(200).json(urls)
+    response.status(200).json(urls);
   })
   .catch((error) => {
-    console.error('error getting URLs:', error)
-  })
-})
+    console.error('error getting URLs:', error);
+  });
+});
 
 app.post('/api/v1/urls', (request, response) => {
-  const { folderId, url } = request.body
-  const id = md5(url)
+  const { folderId, url } = request.body;
+  const id = md5(url);
   const timestamp = Date.now();
-  const numOfClicks = 0
+  const numOfClicks = 0;
   const shortenedUrl = id.slice(0,5);
-  const urlObj = { folderId, numOfClicks, shortenedUrl, url, id, timestamp }
+  const urlObj = { folderId, numOfClicks, shortenedUrl, url, id, timestamp };
 
   database('urls').insert(urlObj)
   .then(function() {
@@ -70,10 +71,10 @@ app.post('/api/v1/urls', (request, response) => {
         response.status(200).json(urls);
       })
       .catch(function(error) {
-        console.error('error posting URLs:', error)
+        console.error('error posting URLs:', error);
       });
-  })
-})
+  });
+});
 
 app.get('/:shortUrl', (request, response) => {
   const { shortUrl } = request.params;
@@ -82,41 +83,40 @@ app.get('/:shortUrl', (request, response) => {
 
   database('urls').where('shortenedUrl', shortUrl).select()
   .then((urlArray) => {
-    longUrl = (urlArray[0].url)
-    newCount = (urlArray[0].numOfClicks) + 1
+    longUrl = (urlArray[0].url);
+    newCount = (urlArray[0].numOfClicks) + 1;
   })
   .then(() => {
     database('urls').where('shortenedUrl', shortUrl).update({ numOfClicks: newCount })
     .then(() => {
       response.status(302).redirect(`${longUrl}`);
-    })
+    });
   })
   .catch((error) => {
-    console.error('error getting short URL:', error)
-  })
-})
+    console.error('error getting short URL:', error);
+  });
+});
 
 app.put('/:shortUrl', (request, response) => {
   const { shortUrl } = request.params;
   let newCount;
 
   database('urls').where('shortenedUrl', shortUrl).select().then((urlArray) => {
-    console.log('urlArray:',urlArray)
-    newCount = (urlArray[0].numOfClicks) + 1
+    newCount = (urlArray[0].numOfClicks) + 1;
   })
   .then(() => {
     database('urls').where('shortenedUrl', shortUrl).update({ numOfClicks: newCount })
     .then((urls) => {
       response.status(200).json(urls);
-    })
+    });
   })
   .catch((error) => {
-    console.error('error getting short URL:', error)
-  })
-})
+    console.error('error getting short URL:', error);
+  });
+});
 
 app.listen(app.get('port'), () => {
-  console.log(`Running on ${app.get('port')}`)
-})
+  console.log(`Running on ${app.get('port')}`);
+});
 
 module.exports = app;
